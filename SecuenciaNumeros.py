3a596dc4c6c574a31f920ee60ebe79b8cefe55b7
Jose Fabian Laverde Triana <jf.laverde@uniandes.edu.co>
2019-03-14 22:20:30 -0500

    1-Corregido Secuencia Numeros con varios numeros (Verde)

--- SecuenciaNumeros.py
+++ SecuenciaNumeros.py
@@ -1,7 +1,9 @@
 
 class SecuenciaNumeros:
     def procesarElementos(self, cadena):
-        if cadena=="":
+        if cadena == "":
             return [0,0,0,0]
-        return [int(cadena),0,0,0]
+        else:
+            numeros = cadena.split(",")
+            return [len(numeros),0,0,0]
 
