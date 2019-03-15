dc1d1e97d960f417472be39c6f89429bf0a3f21c
Rafael <raf_berm2@yahoo.com>
2019-03-14 22:43:22 -0500

    3- Prueba con error y secuencia con un número (rojo)

--- testSecuenciaNumeros.py
+++ testSecuenciaNumeros.py
@@ -9,11 +9,11 @@ class TestSecuenciaNumeros(TestCase):
 
 class TestSecuenciaNumerosConUnNumero(TestCase):
     def test_procesarElementos(self):
-        self.assertEqual(SecuenciaNumeros().procesarElementos("1"), [1,1,0,0], "Secuencia con un numero")
-        self.assertEqual(SecuenciaNumeros().procesarElementos("2"), [1,2,0,0], "Secuencia con un numero")
+        self.assertEqual(SecuenciaNumeros().procesarElementos("1"), [1,1,1,0], "Secuencia con un numero")
+        self.assertEqual(SecuenciaNumeros().procesarElementos("2"), [1,2,2,0], "Secuencia con un numero")
 
 class TestSecuenciaNumerosConMultiplesNumeros(TestCase):
     def test_procesarElementos(self):
-        self.assertEqual(SecuenciaNumeros().procesarElementos("1,2,3,4"), [4, 1, 0, 0], "Secuencia con multiples numeros")
-        self.assertEqual(SecuenciaNumeros().procesarElementos("4,3,2,1"), [4, 1, 0, 0], "Secuencia con multiples numeros")
+        self.assertEqual(SecuenciaNumeros().procesarElementos("1,2,3,4"), [4, 1, 4, 0], "Secuencia con multiples numeros")
+        self.assertEqual(SecuenciaNumeros().procesarElementos("4,3,2,1"), [4, 1, 4, 0], "Secuencia con multiples numeros")
 
