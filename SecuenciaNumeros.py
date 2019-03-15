e6208fe33262a18ec6c0583c51ec52ba467d04af
jaruano <jairo.ruano@kantartns.com>
2019-03-14 23:19:58 -0500

    5 - corregido test con multiples numeros y dos puntos como delimitador (verde)

--- SecuenciaNumeros.py
+++ SecuenciaNumeros.py
@@ -5,6 +5,7 @@ class SecuenciaNumeros:
             return [0,0,0,0]
         else:
             cadena = cadena.replace("&",",")
+            cadena = cadena.replace(":",",")
             numeros = cadena.split(",")
             intNumeros = list(map(int, numeros))
 
